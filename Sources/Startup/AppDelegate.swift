7ac21f6f2f1d8b315d31db03cfe0a10a2517daaf
Marcus Vinícius <veras.marcusvinicius@gmail.com>
2019-09-03 21:45:05 -0300

    project-folders-organization

--- Sources/Startup/AppDelegate.swift
+++ Sources/Startup/AppDelegate.swift
@@ -10,12 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let controller: UIViewController = ViewController()
+        let navigationController: UINavigationController = UINavigationController(rootViewController: controller)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = navigationController
+        
         return true
     }
 
@@ -40,7 +45,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
